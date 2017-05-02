7cd3789953b29a43eb0954f94eaded4683c354e8
JoMingyu <city7310@naver.com>
2017-05-02 21:06:42 +0900

    어노테이션 추가

--- src/com/planb/networking/simple/exceptions/TargetAddressNotDeclaredException.java
+++ src/com/planb/networking/simple/exceptions/TargetAddressNotDeclaredException.java
@@ -1,5 +1,6 @@
 package com.planb.networking.simple.exceptions;
 
+@SuppressWarnings("serial")
 public class TargetAddressNotDeclaredException extends Exception {
 	public TargetAddressNotDeclaredException() {
 		super("요청 주소가 지정되지 않았습니다.");
